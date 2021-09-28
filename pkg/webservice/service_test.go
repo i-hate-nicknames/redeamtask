c0fe370652cbfb4f95a500076b26f06e4f0cdf81
Never M <nevermn@protonmail.com>
2021-09-28 13:14:15 +0300

    Add test for Create book operation

--- pkg/webservice/service_test.go
+++ pkg/webservice/service_test.go
@@ -2,13 +2,16 @@ package webservice
 
 import (
 	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/i-hate-nicknames/redeamtask/pkg/api"
+	"github.com/i-hate-nicknames/redeamtask/pkg/book"
 	"github.com/i-hate-nicknames/redeamtask/pkg/db"
 )
 
@@ -26,5 +29,27 @@ func TestInvalidPath(t *testing.T) {
 
 	service.ServeHTTP(w, r)
 	assert.Equal(t, http.StatusNotFound, w.Code, w.Body.String())
+}
 
+func TestCreateBook(t *testing.T) {
+	service := newService()
+	w := httptest.NewRecorder()
+	b := book.Book{
+		Title:       "title",
+		Author:      "author",
+		Publisher:   "publisher",
+		PublishDate: time.Now(),
+		Rating:      1,
+		Status:      book.StatusCheckedOut,
+	}
+	payload, _ := json.Marshal(b)
+	body := bytes.NewBuffer(payload)
+	r := httptest.NewRequest(http.MethodPost, "/book", body)
+
+	service.ServeHTTP(w, r)
+	assert.Equal(t, http.StatusOK, w.Code, w.Body.String())
+
+	// todo: unmarshal response and test that it's valid
+
+	// todo: request the book back
 }
