414b14cacafaa7880179d4a2c46559e7b259a5c5
Never M <nevermn@protonmail.com>
2021-09-28 13:48:14 +0300

    Add memory db operations

--- pkg/db/memory.go
+++ pkg/db/memory.go
@@ -14,12 +14,21 @@ func MakeMemoryDB() BookDB {
 	return db
 }
 
-func (db *memoryDB) Save(*BookRecord) error {
-	panic("not implemented")
+func (db *memoryDB) Save(br *BookRecord) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	db.items[br.id] = br
+	return nil
 }
 
 func (db *memoryDB) Get(id int) (*BookRecord, error) {
-	panic("not implemented")
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+	b, ok := db.items[id]
+	if !ok {
+		return nil, ErrBookNotFound
+	}
+	return b, nil
 }
 
 func (db *memoryDB) Close() error {
