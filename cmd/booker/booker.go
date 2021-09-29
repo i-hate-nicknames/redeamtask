f733bb0336708d09ba5120f48e7dec000bcd67cb
Never M <nevermn@protonmail.com>
2021-09-29 15:18:30 +0300

    Implement CRUD postgres operations

--- cmd/booker/booker.go
+++ cmd/booker/booker.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/i-hate-nicknames/redeamtask/pkg/api"
@@ -9,8 +12,16 @@ import (
 )
 
 func main() {
-	bookDB := db.MakeMemoryDB()
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", "localhost", 8035, "root", "pass", "booker")
+	bookDB, err := db.MakePostgresDB(context.Background(), dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer bookDB.Close()
+	err = bookDB.Migrate()
+	if err != nil {
+		log.Fatal(err)
+	}
 	bookAPI := api.NewAPI(bookDB)
 	service := webservice.MakeService(bookAPI)
 	http.ListenAndServe(":8080", service)
--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -2,6 +2,12 @@ package db
 
 import (
 	"context"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+
+	_ "embed"
 
 	"github.com/jackc/pgx/v4"
 )
@@ -19,25 +25,89 @@ func MakePostgresDB(ctx context.Context, dsn string) (BookDB, error) {
 }
 
 func (db *postgresDB) Migrate() error {
-	panic("not implemented")
+	// todo: a migration library would be nice
+	f, err := os.Open("sql/1-books.sql")
+	if err != nil {
+		return err
+	}
+	migration, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+	_, err = db.conn.Exec(context.TODO(), string(migration))
+	return err
 }
 
 func (db *postgresDB) Create(br *BookRecord) (*BookRecord, error) {
-	panic("not implemented")
+	query := `
+		INSERT INTO books (title, author, publisher, publish_date, rating, _status)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING id
+	`
+	lastInsertedID := 0
+	err := db.conn.QueryRow(context.TODO(), query, br.Title, br.Author, br.Publisher, br.PublishDate, br.Rating, br.Status).Scan(&lastInsertedID)
+	if err != nil {
+		return nil, err
+	}
+	br.ID = lastInsertedID
+	return br, nil
 }
 
 func (db *postgresDB) Update(br *BookRecord) error {
-	panic("not implemented")
+	query := `
+		UPDATE books
+		SET
+			title = $1,
+			author = $2,
+			publisher = $3,
+			publish_date = $4,
+			rating = $5,
+			_status = $6
+		WHERE id = $7 AND deleted_at IS NULL
+	`
+	cmd, err := db.conn.Exec(context.TODO(), query, br.Title, br.Author, br.Publisher, br.PublishDate, br.Rating, br.Status, br.ID)
+	affected := cmd.RowsAffected()
+	if affected == 0 {
+		return ErrBookNotFound
+	}
+	if affected > 1 {
+		log.Println("Single book update affected more than a single book")
+	}
+	return err
 }
 
-func (db *postgresDB) Get(id int) (*BookRecord, error) {
-	panic("not implemented")
+func (db *postgresDB) Get(ID int) (*BookRecord, error) {
+	query := `
+		SELECT id, title, author, publisher, publish_date, rating, _status
+		FROM books
+		WHERE id = $1 AND deleted_at IS NULL
+	`
+	var br BookRecord
+	row := db.conn.QueryRow(context.TODO(), query, ID)
+	err := row.Scan(&br.ID, &br.Title, &br.Author, &br.Publisher, &br.PublishDate, &br.Rating, &br.Status)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, ErrBookNotFound
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &br, nil
 }
 
 func (db *postgresDB) Delete(ID int) error {
-	panic("not implemented")
+	query := `
+		UPDATE books
+		SET deleted_at = now()
+		WHERE id = $1 AND deleted_at IS NULL
+	`
+	cmd, err := db.conn.Exec(context.TODO(), query, ID)
+	affected := cmd.RowsAffected()
+	if affected > 1 {
+		log.Println("Single book delete affected more than a single book")
+	}
+	return err
 }
 
 func (db *postgresDB) Close() error {
-	panic("not implemented")
+	return db.conn.Close(context.TODO())
 }
