ef1042c8730aa2558b1f02cf71d173ed45dc59b9
Never M <nevermn@protonmail.com>
2021-09-29 15:18:13 +0300

    Uppercase migration sql

--- sql/1-books.sql
+++ sql/1-books.sql
@@ -1,30 +1,30 @@
 -- running CREATE TYPE multiple times throws an error
--- todo: when migrations are properly implemented call create type directly
-create or replace function create_book_status_type()
-    returns boolean
-    language plpgsql
-as
+-- todo: when migrations are properly implemented call CREATE type directly
+CREATE OR replace FUNCTION create_book_status_type()
+    RETURNS boolean
+    LANGUAGE plpgsql
+AS
 $$
-begin
-  if not exists(select 1 from pg_type where typname = 'book_status') then
-      create type book_status as enum ('checked_out', 'checked_in');
-  end if;
-  return true;
-end;
+BEGIN
+  IF NOT EXISTS(SELECT 1 FROM pg_type WHERE typname = 'book_status') THEN
+      CREATE TYPE book_status AS enum ('checked_out', 'checked_in');
+  END IF;
+  RETURN true;
+END;
 $$;
 
-select create_book_status_type();
+SELECT create_book_status_type();
 
-create table if not exists books (
-    id bigserial primary key,
-    title text not null,
-    author text not null,
-    publisher text not null,
-    publish_date time not null,
-    rating smallint not null CHECK (rating >= 1 AND rating <= 3),
-    _status book_status not null,
-    created_at timestamp(0) with time zone not null default now(),
-    deleted_at timestamp(0)
+CREATE TABLE IF NOT EXISTS books (
+    id bigserial PRIMARY KEY,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    publisher TEXT NOT NULL,
+    publish_date TIME NOT NULL,
+    rating SMALLINT NOT NULL CHECK (rating >= 1 AND rating <= 3),
+    _status book_status NOT NULL,
+    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL default NOW(),
+    deleted_at TIMESTAMP(0)
 );
 
-create index if not exists books_deleted on books (deleted_at);
+CREATE index IF NOT EXISTS books_deleted ON books (deleted_at);
