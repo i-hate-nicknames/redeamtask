b83a208b1962c4a1996f0fe8233809f7e6112b49
Never M <nevermn@protonmail.com>
2021-09-29 15:11:59 +0300

    Fix migration to be safe to run multiple times

--- sql/1-books.sql
+++ sql/1-books.sql
@@ -1,4 +1,19 @@
-create type book_status as enum ('checked_out', 'checked_in');
+-- running CREATE TYPE multiple times throws an error
+-- todo: when migrations are properly implemented call create type directly
+create or replace function create_book_status_type()
+    returns boolean
+    language plpgsql
+as
+$$
+begin
+  if not exists(select 1 from pg_type where typname = 'book_status') then
+      create type book_status as enum ('checked_out', 'checked_in');
+  end if;
+  return true;
+end;
+$$;
+
+select create_book_status_type();
 
 create table if not exists books (
     id bigserial primary key,
@@ -12,4 +27,4 @@ create table if not exists books (
     deleted_at timestamp(0)
 );
 
-create index books_deleted on books (deleted_at);
+create index if not exists books_deleted on books (deleted_at);
